c6352453bc5379796429764952d339599b177fdd
Jarosław Weselski <jweselski@objectivity.co.uk>
2018-11-28 10:11:31 +0100

    Add basic routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import {RecipesComponent} from './recipes/recipes.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: '/recipes',
+        pathMatch: 'full'
+    },
+    {
+        path: 'recipes',
+        component: RecipesComponent
+    },
+    {
+        path: 'shoppingList',
+        component: ShoppingListComponent
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {FormsModule} from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {RecipeService} from './recipes/recipe.service';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import {RecipeService} from './recipes/recipe.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
