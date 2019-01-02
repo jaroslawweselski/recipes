3a00bdb256b4eda4f345bfe56b45fe113fe35dcf
Jarosław Weselski <jweselski@objectivity.co.uk>
2019-01-02 16:23:49 +0100

    Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,55 +1,20 @@
-import {RecipesComponent} from './recipes/recipes.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {SignupComponent} from './auth/signup/signup.component';
-import {SigninComponent} from './auth/signin/signin.component';
-import {AuthGuardService} from './auth/auth-guard.service';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {HomeComponent} from './home/home.component';
 
 const appRoutes: Routes = [
     {
         path: '',
-        redirectTo: '/recipes',
-        pathMatch: 'full'
+        component: HomeComponent
     },
     {
         path: 'recipes',
-        component: RecipesComponent,
-        children: [
-            {
-                path: '',
-                component: RecipeStartComponent
-            },
-            {
-                path: 'new',
-                component: RecipeEditComponent,
-                canActivate: [AuthGuardService]
-            },
-            {
-                path: ':id',
-                component: RecipeDetailComponent
-            },
-            {
-                path: ':id/edit',
-                component: RecipeEditComponent,
-                canActivate: [AuthGuardService]
-            }
-        ]
+        loadChildren: './recipes/recipes.module#RecipesModule'
     },
     {
-        path: 'shoppingList',
-        component: ShoppingListComponent
-    },
-    {
-        path: 'signup',
-        component: SignupComponent
-    },
-    {
-        path: 'signin',
-        component: SigninComponent
+      path: 'shoppingList',
+      component: ShoppingListComponent
     }
 ];
 
